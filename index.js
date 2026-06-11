c2012db1bf313b93576d707931d4c5ae2763cb2f
ㅤㅤㅤㅤㅤㅤ <sdkuralsiz@gmail.com>
2026-06-12 00:21:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const { Client, GatewayIntentBits, Partials, Collection, Events } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildVoiceStates // Ses logları için şart
+    ],
+    partials: [
+        Partials.Message, 
+        Partials.Channel, 
+        Partials.GuildMember, 
+        Partials.User
+    ] // Eski mesajların loglanabilmesi için şart!
+});
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+    const folderPath = path.join(commandsPath, folder);
+    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(folderPath, file);
+        const command = require(filePath);
+        if ('name' in command && 'execute' in command) {
+            client.commands.set(command.name, command);
+            if (command.aliases && Array.isArray(command.aliases)) {
+                command.aliases.forEach(alias => client.aliases.set(alias, command.name));
+            }
+        }
+    }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.login(config.token);
